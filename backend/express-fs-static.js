e36b7fd51c4bedbdaa53c18fd77c5da1d3c96e2c
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2022-07-04 11:25:38 -0300

    :sparkles feat: server routes

---
+++ backend/express-fs-static.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const router = express.Router();
+
+app.get('/', function(req, res){
+    const content = fs.readFileSync(__dirname + '/static/index.html', 'utf-8')
+    res.send(content)
+})
+
+app.listen(4000, () => {
+    console.log('server listen on http://localhost:4000')
+});
+
+app.use(bodyParser.json())
+
+router.route("/")
+    .all((req, res, next) => {
+        console.log(`Request from : ${req.originalUrl}`)
+        console.log(`Request type : ${req.method}`)
+        console.log(`Request params : ${req.params}`)
+        next();
+    })
+    .get((req, res) => {
+        console.log(req.params)
+        res.send(JSON.stringify([]))
+        res.status(200)
+        res.end()
+    })
+    .post((req, res) => {
+        console.log(JSON.stringify(req.body))
+        res.status(201)
+        res.end()
+    })
+
+router
+    .param('id', (req, res, next, id) => {
+        console.log(`Request from : ${req.params}`)
+        console.log(`Request type : ${req.method}`)
+        console.log(`Request id : ${id}`)
+        next()
+    })
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.params)
+        res.send(JSON.stringify({}))
+        res.status(200)
+        res.end()
+    })
+    .put((req, res) => {
+        console.log(req.body)
+        res.status(203)
+        res.end()
+    })
+    .delete((req, res) => {
+        res.status(203)
+        res.end()
+    })
+
+
+app.use('/posts', router)
